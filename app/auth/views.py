bcb481ecbe762c366480c0108dead8f1e29f25a7
suboqi <422819555@qq.com>
2017-04-14 18:08:13 +0800

    帐户注册添加发送确认邮件 P92

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding=utf-8
 from flask import render_template, session, redirect, url_for, request, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, current_user, login_required
 from . import auth
 from ..models import User, db
 from forms import LoginForm, RegistrationForm
+from ..email import send_email
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -33,7 +34,22 @@ def register():
     if form.validate_on_submit():
         user = User(email=form.email.data, username=form.username.data,
                     password=form.password.data)
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account', 'auth/email/confirm',
+                   user=user, token=token)
         db.session.add(user)
         flash('You can now login.')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
